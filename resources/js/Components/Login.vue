209dd1fa2d024e569f0d61e6073ce53227894edf
Muhammad Kashif zia <kashif.idenbrid@gmail.com>
2022-11-10 09:46:58 +0500

    login page

--- resources/js/Components/Login.vue
+++ resources/js/Components/Login.vue
@@ -1,3 +1,41 @@
 <template>
-    <h1>Login Form</h1>
+   <div class="limiter">
+		<div class="container-login100" style="background-image: url(./assets/images/bg-img.png);">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-49">
+						<img src="/assets/images/logo.png" class="login-logo">
+					</span>
+
+					<div class="wrap-input100">
+						<span class="label-input100">会社ID</span>
+						<input class="input100" type="text" name="username">
+					
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<span class="label-input100">個人ID</span>
+						<input class="input100" type="password" name="pass">
+					
+					</div>
+				
+					
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">
+								ログイン
+							</button>
+						</div>
+					</div>
+
+				</form>
+			
+				<div>
+					<img src="/assets/images/lcm-logo.png" class="lcm-logo">
+				</div>
+			</div>
+		</div>
+	</div>  
 </template>
